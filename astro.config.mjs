4769b081ea6daf7e2e86748bea8bbcc236c45952
mrifkitrisaputra <mrifki2309@gmail.com>
2024-04-19 02:42:24 +0700

    Bismillah deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tunnel from "astro-tunnel";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   renderers: ["@astrojs/renderer-react", "@astrojs/renderer-svelte"],
   integrations: [tailwind(), react(), tunnel()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
